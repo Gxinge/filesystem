fd5843461b69e915bd2ba9a80700e9fc94694dd8
gaoxin9601 <gaoxin9601@runoob.com>
2022-09-26 17:35:24 +0800

    Initial commit

---
+++ function/function.go
@@ -0,0 +1,74 @@
+package function
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//写入新文件
+func write(path string , p []byte){
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
+	if err!= nil {
+		fmt.Println("写入文件失败")
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	writer.Write(p)
+
+	writer.Flush()
+
+}
+
+//追加写文件
+func writeAppend(path string , p []byte){
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, 0666)
+	if err!= nil {
+		fmt.Println("写入文件失败")
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	writer.Write(p)
+
+	writer.Flush()
+}
+
+
+
+//读取文件
+func read(in string) []byte {
+	file, err := os.OpenFile(in, os.O_RDWR|os.O_APPEND, 0666)
+	if err!= nil {
+		fmt.Println("读文件失败")
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	buf := make([]byte, 1024)
+	//读取 Reader 对象中的内容到 []byte 类型的 buf 中
+	reader.Read(buf)
+	return buf
+}
+
+//创建文件夹
+func mkdir(path string){
+	os.MkdirAll(path,os.ModePerm)
+}
+
+//删除文件(夹）
+func delete(path string){
+	err := os.RemoveAll(path)
+	if err != nil{
+		fmt.Println("文件删除失败",err)
+	}
+}
+
+//创建数据集
+func dataSet(path string){
+	path = "/dataset"+ path
+	os.MkdirAll(path,os.ModePerm)
+}
